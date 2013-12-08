a8d12b087f0490f1a7df300ba23948d15c528381
Ethan Woo <ethan.woo@gmail.com>
2013-12-07 22:30:45 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PangeaEdu::Application.config.secret_key_base = '4373e8390ae6eff7ee308e1240fa1c5ef3dc08d36ba513599263771e1af9a7567b22e9569357a1d27bd35c495bcb5d9f9bccac1626693ea8603492bfda58edc3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PangeaEdu::Application.config.session_store :cookie_store, key: '_pangea_edu_session'
