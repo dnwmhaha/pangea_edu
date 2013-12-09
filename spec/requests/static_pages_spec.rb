142e91df1d9c9097dd6768271cc67dfda8f9ed70
Ethan Woo <ethan.woo@gmail.com>
2013-12-08 22:44:07 -0500

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Welcome to Pangea Physics'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Welcome to Pangea Physics')
+    end
+  
+
+   it "should have the title 'Pangea Physics - Everything is connected'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Pangea Physics - Everything is connected")
+    end
+  end
+  
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+  
+
+   it "should have the title 'Pangea Physics - Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Pangea Physics - Contact Us")
+    end
+  end
+
+
+end
